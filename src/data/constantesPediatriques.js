47b288dfacfcbd5d43053224196dd9fa7c2f5422
Georges Lecomte <glecomte.ide@gmail.com>
2026-05-18 15:08:04 +0200

    adaptation âge max dans fourchette constantes pédiatriques

--- src/data/constantesPediatriques.js
+++ src/data/constantesPediatriques.js
@@ -1,12 +1,12 @@
 const constantesPediatriques = [
   { ageMin: 1, ageMax: 1.99, tranche: "1 an", poids: "10 kg", fc: "105 – 150 /min", fr: "20 – 45 /min", pas: "70 – 105 mmHg", pad: "37 – 67 mmHg", pam: "53 – 81 mmHg" },
-  { ageMin: 2, ageMax: 3.99, tranche: "2 – 3 ans", poids: "12 – 14 kg", fc: "95 – 150 /min", fr: "20 – 40 /min", pas: "70 – 105 mmHg", pad: "37 – 67 mmHg", pam: "53 – 81 mmHg" },
-  { ageMin: 4, ageMax: 5.99, tranche: "4 – 5 ans", poids: "15 – 18 kg", fc: "80 – 150 /min", fr: "17 – 30 /min", pas: "75 – 110 mmHg", pad: "50 – 74 mmHg", pam: "63 – 87 mmHg" },
-  { ageMin: 6, ageMax: 7.99, tranche: "6 – 7 ans", poids: "20 – 23 kg", fc: "75 – 140 /min", fr: "16 – 30 /min", pas: "80 – 115 mmHg", pad: "50 – 74 mmHg", pam: "63 – 87 mmHg" },
-  { ageMin: 8, ageMax: 9.99, tranche: "8 – 9 ans", poids: "25 – 28 kg", fc: "70 – 130 /min", fr: "16 – 30 /min", pas: "80 – 115 mmHg", pad: "57 – 80 mmHg", pam: "70 – 94 mmHg" },
-  { ageMin: 10, ageMax: 11.99, tranche: "10 – 11 ans", poids: "30 – 35 kg", fc: "60 – 130 /min", fr: "15 – 25 /min", pas: "85 – 120 mmHg", pad: "57 – 80 mmHg", pam: "70 – 94 mmHg" },
-  { ageMin: 12, ageMax: 13.99, tranche: "12 – 13 ans", poids: "40 – 45 kg", fc: "65 – 120 /min", fr: "15 – 25 /min", pas: "90 – 120 mmHg", pad: "62 – 87 mmHg", pam: "76 – 103 mmHg" },
-  { ageMin: 14, ageMax: 15.99, tranche: "14 – 15 ans", poids: "50 – 55 kg", fc: "60 – 115 /min", fr: "14 – 25 /min", pas: "90 – 125 mmHg", pad: "62 – 87 mmHg", pam: "76 – 103 mmHg" },
+  { ageMin: 2, ageMax: 3.99, tranche: "2 – 4 ans", poids: "12 – 14 kg", fc: "95 – 150 /min", fr: "20 – 40 /min", pas: "70 – 105 mmHg", pad: "37 – 67 mmHg", pam: "53 – 81 mmHg" },
+  { ageMin: 4, ageMax: 5.99, tranche: "4 – 6 ans", poids: "15 – 18 kg", fc: "80 – 150 /min", fr: "17 – 30 /min", pas: "75 – 110 mmHg", pad: "50 – 74 mmHg", pam: "63 – 87 mmHg" },
+  { ageMin: 6, ageMax: 7.99, tranche: "6 – 8 ans", poids: "20 – 23 kg", fc: "75 – 140 /min", fr: "16 – 30 /min", pas: "80 – 115 mmHg", pad: "50 – 74 mmHg", pam: "63 – 87 mmHg" },
+  { ageMin: 8, ageMax: 9.99, tranche: "8 – 10 ans", poids: "25 – 28 kg", fc: "70 – 130 /min", fr: "16 – 30 /min", pas: "80 – 115 mmHg", pad: "57 – 80 mmHg", pam: "70 – 94 mmHg" },
+  { ageMin: 10, ageMax: 11.99, tranche: "10 – 12 ans", poids: "30 – 35 kg", fc: "60 – 130 /min", fr: "15 – 25 /min", pas: "85 – 120 mmHg", pad: "57 – 80 mmHg", pam: "70 – 94 mmHg" },
+  { ageMin: 12, ageMax: 13.99, tranche: "12 – 14 ans", poids: "40 – 45 kg", fc: "65 – 120 /min", fr: "15 – 25 /min", pas: "90 – 120 mmHg", pad: "62 – 87 mmHg", pam: "76 – 103 mmHg" },
+  { ageMin: 14, ageMax: 15.99, tranche: "14 – 16 ans", poids: "50 – 55 kg", fc: "60 – 115 /min", fr: "14 – 25 /min", pas: "90 – 125 mmHg", pad: "62 – 87 mmHg", pam: "76 – 103 mmHg" },
   { ageMin: 16, ageMax: 18, tranche: "16 – 18 ans", poids: "60 – 65 kg", fc: "60 – 115 /min", fr: "14 – 25 /min", pas: "90 – 130 mmHg", pad: "62 – 87 mmHg", pam: "76 – 103 mmHg" },
 ];
 
